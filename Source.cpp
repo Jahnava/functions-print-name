110f1522fde7a652c02cea1778024b240ef8e612
Jahnava <jahnavalmt@gmail.com>
2020-10-15 15:31:56 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+//prototypes go here
+void displayName();
+
+int main()
+{
+	//display my name on console screen
+	displayName();
+
+	return 0;
+}
+
+//********************************
+//display my name on the output
+//********************************
+
+void displayName()
+{
+	cout << "My name is Jahnava Baltz" << endl << endl;
+}
